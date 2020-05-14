a51f929ef7ce11e1e267f83706bb2966b8c44dd7
zhangh <506365426@qq.com>
2020-05-14 23:17:01 +0800

    docs: 电影选项卡页面豆瓣数据获取

--- we-chat/pages/movies/movies.js
+++ we-chat/pages/movies/movies.js
@@ -1,17 +1,24 @@
 // pages/movies/movies.js
+var app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    movies: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    var inTheatersUrl = app.globalData.doubanBase +
+      "/v2/movie/in_theaters" + "?start=0&count=3";
+    this.getMovieListData(inTheatersUrl);
+  },
+  getMovieListData: function (url) {
+    var that = this;
     wx.request({
       url: 'http://t.yushu.im/v2/movie/top250?start=0&count=3',
       method: 'GET',
@@ -19,63 +26,36 @@ Page({
         'Content-type': 'application/json'
       },
       success: function (res) {
-        console.log(res)
+        // console.log(res)
+        that.processDoubanData(res.data)
       },
       fail: function () {
         console.info('failed')
       },
       complete: function () {
-
+        console.info('complete')
       }
     })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function (event) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  processDoubanData: function (moviesDouban) {
+    var movies = [];
+    for (var idx in moviesDouban.subjects) {
+      var subject = moviesDouban.subjects[idx];
+      var title = subject.title;
+      if (title.length >= 6) {
+        title = title.substring(0, 6) + "...";
+      }
+      var temp = {
+        title: title,
+        average: subject.rating.average,
+        coverageUrl: subject.images.large,
+        movieId: subject.id
+      }
+      movies.push(temp)
+    }
+    console.info(movies)
+    this.setData({
+      movies: movies
+    })
   }
 })
